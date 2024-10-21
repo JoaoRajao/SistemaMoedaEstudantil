46f8523a209ccc68b369a9e04fb8aea40c83c2be
OtavioSalomao <otavio.ferreira.mds@gmail.com>
2024-10-20 21:37:52 -0300

    Refactor VantagemController to use DTO in API endpoints

--- Implementacao/MoedaEstudantil/MoedaEstudantil/Entities/Vantagem.cs
+++ Implementacao/MoedaEstudantil/MoedaEstudantil/Entities/Vantagem.cs
@@ -1,11 +1,40 @@
-﻿namespace MoedaEstudantil.Entities
+﻿using MoedaEstudantil.DTOs;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoedaEstudantil.Entities
 {
     public class Vantagem
     {
-        public int Id { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("Empresa")]
+        public Guid EmpresaId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Descricao { get; set; }
-        public decimal Custo { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Foto { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Custo { get; set; }
+
+        public static Vantagem FromDTO(VantagemDTO dto)
+        {
+            return new Vantagem
+            {
+                Id = Guid.NewGuid(),
+                EmpresaId = dto.EmpresaID,
+                Descricao = dto.Descricao,
+                Custo = dto.Custo,
+                Foto = dto.Foto
+            };
+        }
     }
 
 }
---
+++ Implementacao/MoedaEstudantil/MoedaEstudantil/Services/VantagemService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoedaEstudantil.Data;
+using MoedaEstudantil.DTOs;
+using MoedaEstudantil.Entities;
+
+namespace MoedaEstudantil.Services
+{
+    public class VantagemService
+    {
+        private readonly MeritSystemContext _context;
+
+        public VantagemService(MeritSystemContext context)
+        {
+            _context = context;
+        }
+
+        public Vantagem CadastrarVantagem(VantagemDTO vantagemDto)
+        {
+            var vantagem = Vantagem.FromDTO(vantagemDto);
+            _context.Vantagens.Add(vantagem);
+            _context.SaveChanges();
+            return vantagem;
+        }
+
+        public Vantagem GetVantagemById(Guid id)
+        {
+            return _context.Vantagens
+                .Include(v => v.EmpresaId)
+                .FirstOrDefault(v => v.Id == id);
+        }
+
+        public bool AtualizarVantagem(Vantagem atualizado)
+        {
+            var vantagem = _context.Vantagens.Find(atualizado.Id);
+            if (vantagem == null)
+                return false;
+
+            // Atualizar propriedades
+            vantagem.Descricao = atualizado.Descricao;
+            vantagem.Custo = atualizado.Custo;
+
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool DeletarVantagem(Guid id)
+        {
+            var vantagem = _context.Vantagens.Find(id);
+            if (vantagem == null)
+                return false;
+
+            _context.Vantagens.Remove(vantagem);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public List<Vantagem> ListarVantagens()
+        {
+            return _context.Vantagens.Include(v => v.EmpresaId).ToList();
+        }
+    }
+}

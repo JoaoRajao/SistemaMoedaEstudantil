4325bcf913ac2f2116aefcc6b46b63a446f7d94b
OtavioSalomao <otavio.ferreira.mds@gmail.com>
2024-10-20 21:37:38 -0300

    Refactor ProfessorController to use DTO in API endpoints

--- Implementacao/MoedaEstudantil/MoedaEstudantil/Entities/Professor.cs
+++ Implementacao/MoedaEstudantil/MoedaEstudantil/Entities/Professor.cs
@@ -1,11 +1,21 @@
 ﻿using MoedaEstudantil.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MoedaEstudantil.Entities
 {
     public class Professor : Pessoa
     {
+        [Required]
+        [StringLength(100)]
         public string Departamento { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Instituicao { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal SaldoMoedas { get; set; }
 
         public List<Transacao> Transacoes { get; set; }
@@ -14,33 +24,6 @@ namespace MoedaEstudantil.Entities
         {
             Transacoes = new List<Transacao>();
             SaldoMoedas = 1000; // Inicia com 1000 moedas por semestre
-        }
-
-        public void EnviarMoedas(Aluno aluno, decimal quantidade, string mensagem)
-        {
-            if (SaldoMoedas >= quantidade)
-            {
-                SaldoMoedas -= quantidade;
-                aluno.SaldoMoedas += quantidade;
-
-                var transacao = new Transacao
-                {
-                    TipoTransacao = TipoTransacao.ENVIO,
-                    Valor = quantidade,
-                    Data = DateTime.Now,
-                    Mensagem = mensagem,
-                    Participante = aluno.Nome
-                };
-
-                Transacoes.Add(transacao);
-                aluno.Transacoes.Add(transacao);
-
-                // Enviar notificação para o aluno por email
-            }
-            else
-            {
-                throw new Exception("Saldo insuficiente.");
-            }
         }
     }
 

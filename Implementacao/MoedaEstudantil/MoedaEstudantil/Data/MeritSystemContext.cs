53a94beae27cc6d3b4d253286de87a21b9ea1482
OtavioSalomao <otavio.ferreira.mds@gmail.com>
2024-11-10 17:05:33 -0300

    Add initial data seeding and enum conversion to context
    Enhanced MeritSystemContext with OnModelCreating method to seed
    initial data for Aluno, Professor, Empresa, Vantagem, and Transacao
    entities. Configured TipoTransacao enum to be stored as an integer.
    Seeded sample records for testing and development purposes.

--- Implementacao/MoedaEstudantil/MoedaEstudantil/Data/MeritSystemContext.cs
+++ Implementacao/MoedaEstudantil/MoedaEstudantil/Data/MeritSystemContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MoedaEstudantil.Entities;
+using MoedaEstudantil.Enums;
+using System;
 
 namespace MoedaEstudantil.Data
 {
@@ -12,6 +14,99 @@ namespace MoedaEstudantil.Data
         public DbSet<Transacao> Transacoes { get; set; }
 
         public MeritSystemContext(DbContextOptions<MeritSystemContext> options) : base(options) { }
-    }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Guid alunoId = Guid.NewGuid();
+            Guid empresaId = Guid.NewGuid();
+            Guid professorId = Guid.NewGuid();
+            Guid vantagemId = Guid.NewGuid();
+
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = alunoId,
+                    Nome = "João Silva",
+                    Documento = "12345678901",
+                    Email = "joao.silva@example.com",
+                    Senha = "Senha123", // Considere fazer o hash desta senha
+                    Endereco = "Rua das Flores, 123",
+                    InstituicaoEnsino = "Universidade XYZ",
+                    Curso = "Engenharia",
+                    SaldoMoedas = 100
+                }
+            );
+
+            modelBuilder.Entity<Professor>().HasData(
+                new Professor
+                {
+                    Id = professorId,
+                    Nome = "Dr. Carlos Pereira",
+                    Documento = "11223344556",
+                    Email = "carlos.pereira@example.com",
+                    Senha = "Senha789", // Considere fazer o hash desta senha
+                    Departamento = "Ciências Exatas",
+                    Instituicao = "Universidade XYZ",
+                    SaldoMoedas = 500
+                }
+            );
+
+            modelBuilder.Entity<Empresa>().HasData(
+                new Empresa
+                {
+                    Id = empresaId,
+                    Nome = "Empresa Tech",
+                    Documento = "33445566778",
+                    Email = "contato@empresatech.com",
+                    Senha = "SenhaEmp123", // Considere fazer o hash desta senha
+                    SaldoMoedas = 1000
+                }
+            );
+
+            modelBuilder.Entity<Vantagem>().HasData(
+                new Vantagem
+                {
+                    Id = vantagemId,
+                    Nome = "Desconto em Curso Online",
+                    Descricao = "10% de desconto em qualquer curso online da Empresa Tech.",
+                    Custo = 50,
+                    EmpresaId = empresaId
+                }
+            );
+
+            // Configuração da conversão do enum TipoTransacao
+            modelBuilder.Entity<Transacao>()
+                .Property(t => t.TipoTransacao)
+                .HasConversion<int>();
+
+            modelBuilder.Entity<Transacao>().HasData(
+                new Transacao
+                {
+                    Id = Guid.NewGuid(),
+                    AlunoId = alunoId,
+                    ProfessorId = professorId,
+                    TipoTransacao = TipoTransacao.ENVIO,
+                    Valor = 100,
+                    Data = DateTime.UtcNow,
+                    Mensagem = "Participação em projeto de pesquisa"
+                },
+                new Transacao
+                {
+                    Id = Guid.NewGuid(),
+                    AlunoId = alunoId,
+                    VantagemId = vantagemId,
+                    TipoTransacao = TipoTransacao.RESGATE,
+                    Valor = 50,
+                    Data = DateTime.UtcNow,
+                    Mensagem = "Para melhorar minhas habilidades"
+                }
+            );
+            modelBuilder.Entity<Transacao>()
+                .Property(t => t.TipoTransacao)
+                .HasConversion<int>();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
